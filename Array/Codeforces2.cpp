998fd834df4a231db7c7f6e97ba4021422546718
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-09-21 22:03:30 +0530

    Codeforces2

---
+++ Array/Codeforces2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int minindex(vector<int>a , int n){
+        int min=*min_element(a.begin(),a.end());
+    for(int i=0; i<a.size(); i++){
+        if(a[i]==min){
+            return i;
+        }
+    }
+}
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0; i<a.size(); i++){
+        cin>>a[i];
+    }
+    int new_min=minindex(a,n);
+    a[new_min] +=1;
+    int temp=1;
+        for(int i=0; i<a.size(); i++){
+        temp=temp*a[i];}
+    cout<<temp;
+    }
+    
+}
