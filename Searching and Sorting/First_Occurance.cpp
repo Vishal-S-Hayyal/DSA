75cc5ec2b62b9ec4151e84c95462cb11bc998b78
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2023-09-28 03:50:33 +0530

    28/09/2023

--- Searching and Sorting/First_Occurance.cpp
+++ Searching and Sorting/First_Occurance.cpp
@@ -27,6 +27,8 @@ int main() {
 vector<int> v{1,2,3,4,4,4,4,6,7,8,};
 int target = 4;
 int index = firstocc(v,target);
-cout<<"found first at index "<<index;
+cout<<"found first at index "<<index<<endl;
+auto ans2=lower_bound(v.begin(),v.end(),4);
+cout<<ans2-v.begin();
 return 0;
 }
