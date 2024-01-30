3ffc742b33a583642a5b55d86615990c1f219d2f
Vishal-S-Hayyal <vh1835@srmist.edu.in>
2024-01-31 00:35:51 +0530

    String Functions

---
+++ 07_Character Arrays and Strings/String_Functions.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+
+// string str1 = "yaar tera superstar desi kalakar";
+// string str2 = "stardew";
+
+// if(str1.find(str2) == string::npos){
+//     //npos is a member of string and it means that "not found"
+//     cout<<"Not found"<<endl;
+// }
+// else{
+//     cout<<"Found"<<endl;
+// }
+
+
+// string name = "Vishal Hayyal";
+// string middle = " Kumar";
+// name.insert(6,middle);
+// cout<<"Name is : "<<name<<endl;
+
+
+// string desc = "This is a car";
+// desc.erase(4,3);
+// cout<<desc;
+
+// string str1 = "Vishal";
+// string str2 = "Hayyal";
+
+// cout<<"Before appending"<<endl;
+// cout<<str1<<endl;
+// cout<<str2<<endl;
+
+// //append
+// str1.append(str2);
+
+// cout<<"after appending"<<endl;
+// cout<<str1<<endl;
+// cout<<str2<<endl; 
+
+// string name;
+// string temp = " ";
+
+// cin>>name;
+
+// cout<<"Length of name: "<<name.length()<<endl;
+// cout<<"String is empty or not : "<<temp.empty()<<endl;
+// cout<<"Character at position 0 : "<<name.at(0)<<endl;
+// cout<<"Front character of string : "<<name.front()<<endl;
+// cout<<"Back character of string : "<<name.back()<<endl;
+
+return 0;
+}
